30f5e6d2858e9a9626f4167266f4b65c4797685d
Luca De Luigi <lucadeluigi91@gmail.com>
2020-12-18 12:39:26 +0100

    feat: add denormalize function

--- data/utils.py
+++ data/utils.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 import numpy as np  # type: ignore
 from PIL.Image import Image  # type: ignore
 
@@ -9,3 +11,12 @@ def img2depth(dep_img: Image) -> np.ndarray:
     depth = depth.clip(0.01, 1)
     depth = 1 / depth
     return depth
+
+
+def denormalize(img: np.ndarray, mean: Sequence[float], std: Sequence[float]) -> np.ndarray:
+    denorm_img = img
+    if len(denorm_img.shape) > 3:
+        denorm_img = denorm_img.squeeze()
+    denorm_img = img * std
+    denorm_img = denorm_img + mean
+    return np.uint8(denorm_img * 255)
