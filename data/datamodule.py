90ec81b03bd05f43727c5d5cf08ff3b81f6486d0
Luca De Luigi <lucadeluigi91@gmail.com>
2020-12-18 12:39:51 +0100

    feat: add function to get images from a batch

--- data/datamodule.py
+++ data/datamodule.py
@@ -1,6 +1,7 @@
 from functools import partial
-from typing import Any, List, Union
+from typing import Any, List, Optional, Sequence, Union, cast
 
+import numpy as np
 import torch
 from hesiod import hcfg
 from pytorch_lightning import LightningDataModule
@@ -10,6 +11,7 @@ from torch.utils.data import DataLoader
 from data import ITEM_T
 from data.dataset import Dataset
 from data.transforms import Compose
+from data.utils import denormalize
 
 num_workers = hcfg("num_workers", int)
 build_dataloader = partial(DataLoader, num_workers=num_workers, pin_memory=True)
@@ -21,9 +23,9 @@ class DataModule(LightningDataModule):
 
         transform = Compose([])
 
-        self.train_dst = Dataset(transform)
-        self.val_source_dst = Dataset(transform)
-        self.val_target_dst = Dataset(transform)
+        self.train_dst = Dataset("train_dataset", transform)
+        self.val_source_dst = Dataset("val_source_dataset", transform)
+        self.val_target_dst = Dataset("val_target_dataset", transform)
 
     def train_dataloader(self, *args: Any, **kwargs: Any) -> DataLoader:
         bs = hcfg("train_batch_size", int)
@@ -34,6 +36,9 @@ class DataModule(LightningDataModule):
         val_source_dl = build_dataloader(self.val_source_dst, batch_size=bs, shuffle=True)
         val_target_dl = build_dataloader(self.val_target_dst, batch_size=bs, shuffle=True)
         return [val_source_dl, val_target_dl]
+
+    def __len__(self) -> int:
+        return len(self.train_dataloader())
 
     @staticmethod
     def get_samples(dataloader: DataLoader, num_samples: int) -> ITEM_T:
@@ -58,16 +63,31 @@ class DataModule(LightningDataModule):
         return DataModule.get_samples(self.train_dataloader(), num_samples)
 
     def get_val_source_samples(self, num_samples: int) -> ITEM_T:
-        dataloader = self.val_dataloader()
-        if isinstance(dataloader, list):
-            dataloader = dataloader[0]
-        return DataModule.get_samples(dataloader, num_samples)
+        dataloaders = cast(List, self.val_dataloader())
+        return DataModule.get_samples(dataloaders[0], num_samples)
 
     def get_val_target_samples(self, num_samples: int) -> ITEM_T:
-        dataloader = self.val_dataloader()
-        if isinstance(dataloader, list):
-            dataloader = dataloader[1]
-        return DataModule.get_samples(dataloader, num_samples)
+        dataloaders = cast(List, self.val_dataloader())
+        return DataModule.get_samples(dataloaders[1], num_samples)
 
-    def __len__(self) -> int:
-        return len(self.train_dataloader())
+    @staticmethod
+    def get_images(item: ITEM_T, dataset: Dataset) -> Sequence[Optional[np.ndarray]]:
+        img, sem, dep = [element.squeeze() for element in item]
+
+        denorm_img = denormalize(np.array(img), dataset.mean, dataset.std)
+        colored_sem = dataset.semcmap(np.array(sem)) if sem is not None else None
+        colored_dep = dataset.depcmap(np.array(dep)) if dep is not None else None
+
+        return denorm_img, colored_sem, colored_dep
+
+    def get_train_images(self) -> Sequence[Optional[np.ndarray]]:
+        item = self.get_train_samples(1)
+        return DataModule.get_images(item, self.train_dst)
+
+    def get_val_source_images(self) -> Sequence[Optional[np.ndarray]]:
+        item = self.get_val_source_samples(1)
+        return DataModule.get_images(item, self.val_source_dst)
+
+    def get_val_target_images(self) -> Sequence[Optional[np.ndarray]]:
+        item = self.get_val_target_samples(1)
+        return DataModule.get_images(item, self.val_target_dst)
